8d9a70cc50c068e0284d5dd3f8634e4f2b863269
Nimesh Gurung <nimesh.gurung@publicissapient.com>
2020-02-05 14:22:58 +0000

    Skip call on mount

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
-import { bindActionToPromise, bindThunkToPromise, retryRoot } from './utils';
+import * as React from "react";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import { bindActionToPromise, bindThunkToPromise, retryRoot } from "./utils";
 
 export type SagaFunction = (args?: any) => IterableIterator<any>;
 
 export interface IRetryState {
   retryAttempt: number;
   error: boolean;
-  exception: unknown; 
+  exception: unknown;
   success: boolean;
   loading: boolean;
   response: unknown;
@@ -32,30 +32,35 @@ interface IRetryBaseProps {
 
 interface IRetryThunkProps extends IRetryBaseProps {
   thunk: IRetryThunk | IRetryThunk[];
+  skipCallOnMount: Boolean;
 }
 
 interface IRetrySagaProps extends IRetryBaseProps {
   saga: ISaga | ISaga[];
+  skipCallOnMount: Boolean;
 }
 
 type RetryProps = IRetrySagaProps | IRetryThunkProps;
 
 export class Retry extends React.Component<RetryProps, IRetryState> {
-  
   private initialState = {
     error: false,
     success: false,
     exception: undefined,
     loading: true,
     response: undefined
-  }
-  
+  };
+
   constructor(props: RetryProps) {
     super(props);
-    this.state = {...this.initialState, retryAttempt: 0};
+    this.state = { ...this.initialState, retryAttempt: 0 };
   }
 
   public componentDidMount() {
+    if (this.props.skipCallOnMount) {
+      return;
+    }
+
     this.makeApiCall();
   }
 
@@ -103,7 +108,9 @@ export class Retry extends React.Component<RetryProps, IRetryState> {
       }
 
       if ((this.props as IRetryThunkProps).thunk) {
-        response = await bindThunkToPromise(this.props.dispatch)((this.props as IRetryThunkProps).thunk);
+        response = await bindThunkToPromise(this.props.dispatch)(
+          (this.props as IRetryThunkProps).thunk
+        );
       }
 
       this.setState({
@@ -123,5 +130,5 @@ export class Retry extends React.Component<RetryProps, IRetryState> {
   };
 }
 
-export default connect(null, dispatch => ({dispatch}))(Retry)
+export default connect(null, dispatch => ({ dispatch }))(Retry);
 export { retryRoot };
